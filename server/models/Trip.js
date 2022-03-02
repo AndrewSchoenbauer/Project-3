6b96b2254c65203e41675f46e8bb847d02efbcf8
Andrew <andrewschoenbauer12@gmail.com>
2022-03-02 09:58:45 -0600

    added file and folder structure, and also started working on the models for our database

---
+++ server/models/Trip.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const tripSchema = new Schema(
+    {
+        tripName: {
+            type: String,
+            required: true,
+        },
+        startDate: {
+            type: Date,
+            default: Date.now(),
+        },
+        endDate: {
+            type: Date,
+            // Sets a default value of 12 weeks from now
+            default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+        },
+        User: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+
+        Expense: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Expense'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const Trip = model('Trip', tripSchema);
+
+module.exports = Trip;
