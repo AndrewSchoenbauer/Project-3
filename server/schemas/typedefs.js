347554d3c38bb64597ba5240ee6a3021366ad977
Andrew <andrewschoenbauer12@gmail.com>
2022-03-03 10:27:23 -0600

    created the login feature and set up the connection with the database:

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -17,10 +17,12 @@ const typeDefs = gql`
     user: User
   }
   type Query {
-    users: [User]}
+    users: [User]
+    user(username: String!): User}
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }`
 
   module.exports = typeDefs;
