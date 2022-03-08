dba9dca15be7a8285f3fdd1ab654348444115a07
Kate Neuse <kneu4581@gmail.com>
2022-03-07 20:38:04 -0600

    header update

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Auth from '../../utils/auth';
 
 const Header = () => {
@@ -9,7 +8,7 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <header className="bg-text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
