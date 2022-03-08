2eaf55da0bd2374d19447c72212de23d91c3409a
Kate Neuse <kneu4581@gmail.com>
2022-03-07 20:49:35 -0600

    header updates

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -26,12 +26,14 @@ const Header = () => {
             </>
           ) : (
             <>
+            <div className='btns'>
               <Link className="btn btn-lg btn-info m-2" to="/login">
                 Login
               </Link>
               <Link className="btn btn-lg btn-light m-2" to="/signup">
                 Signup
               </Link>
+              </div>
             </>
           )}
         </div>
